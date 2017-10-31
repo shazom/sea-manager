146c44945953798dd78ac6785de8a2044c643ca1
Shalom Leizerovitch <shalom.l@meuhedet.org>
2017-10-31 17:32:21 +0200

    integrate firebase - initial steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+  AngularFirestore
+} from "angularfire2/firestore";
+import { Class } from "app/shared/models/class";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-
   displayCategory: any;
   classCategory: any;
-  displayOptions = [
-    { id: 1, name: 'בכיתה' }, { id: 2, name: 'במגמת' }];
+  displayOptions = [{ id: 1, name: "בכיתה" }, { id: 2, name: "במגמת" }];
   classOptions = [
-    { id: 1, name: 'יא-1' }, { id: 2, name: 'יא-2' }, { id: 3, name: 'יא-3' }];
+    { id: 1, name: "יא-1" },
+    { id: 2, name: "יא-2" },
+    { id: 3, name: "יא-3" }
+  ];
 
   helperFilter: any;
   helperOptions = [
-    { id: 1, name: 'תוספת זמן' },
-    { id: 2, name: 'הקראה בקול' },
-    { id: 3, name: 'מבחן מותאם' }
-  ]
+    { id: 1, name: "תוספת זמן" },
+    { id: 2, name: "הקראה בקול" },
+    { id: 3, name: "מבחן מותאם" }
+  ];
+
+  classes: AngularFirestoreCollection<Class>;
+
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit(): void {
     this.displayCategory = this.displayOptions[0];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { SuiModule } from 'ng2-semantic-ui';
+import { SuiModule } from "ng2-semantic-ui";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
-import { AppComponent } from './app.component';
+import { firebaseConfig } from "app/core/core.module";
+import { AngularFireModule } from "angularfire2";
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     SuiModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule.enablePersistence(),
     HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
